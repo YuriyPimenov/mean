6d6d5d6e92dfb8698017bbb78d752946390e6b00
YuriyPimenov <github1991>
2018-08-16 23:29:30 +0500

    Не много разбили сервер и скачали nodemon

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const express = require('express')
+const app = require('./app')
+const port = process.env.PORT || 5000
 
-const app = express()
-
-app.get('/', (req,res)=>{
-    res.status(200).json({
-        message:'hello gays'
-    })
-})
-
-app.listen(5000,()=>console.log('server start'))
+app.listen(port,()=>console.log(`server start ${port}`))
