7ce734e691cd0cafd159801de96b766184aad45f
YuriyPimenov <github1991>
2018-08-18 11:46:22 +0500

    Добавление всех роутов и контроллеров

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const authRouter = require('./routes/auth')
+const analyticsRouter = require('./routes/analytics')
+const orderRouter = require('./routes/order')
+const positionRouter = require('./routes/position')
+const categoryRouter = require('./routes/category')
+
 const app = express()
 
 app.use('/api/auth', authRouter)
+app.use('/api/analytics', analyticsRouter)
+app.use('/api/order', orderRouter)
+app.use('/api/position', positionRouter)
+app.use('/api/category', categoryRouter)
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./app')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5003
 
 app.listen(port,()=>console.log(`server start ${port}`))
