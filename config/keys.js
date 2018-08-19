a2d5753e4cc39db92c4e73647fbbae2703babd5a
YuriyPimenov <github1991>
2018-08-19 22:15:14 +0500

    Реализуем Аутентификацию, создаем токен jwt

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     // mongoURI:'mongodb://admin:admin12345@ds125352.mlab.com:25352/crm-cafe'
     // mongoURI:'mongodb://admin2:admin2@ds125352.mlab.com:25352/crm-cafe'
-    mongoURI : 'mongodb://admin:admin12345@ds163701.mlab.com:63701/fullstack-block'
+    mongoURI : 'mongodb://admin:admin12345@ds163701.mlab.com:63701/fullstack-block',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,38 @@
 //Для паролей
 const bcrypt = require('bcryptjs')
+//Генерация токена
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
-module.exports.login = (req,res)=>{
-    res.status(200).json({
-        login:{
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async (req,res)=>{
+    const candidate = await User.findOne({email: req.body.email})
+    if(candidate){
+        //Пользователь такой есть
+        //Проверка пароля
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if(passwordResult){
+            //Генерация токена, пароли совпали
+            const token = jwt.sign({
+                email: candidate.email,//Первый параметр это ключи которвый
+                userId: candidate._id//можно зашифровать в токене
+            },keys.jwt,//Секретный ключ
+            {expiresIn: 60*60})//третий, время жизни токена - один час
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        }else{
+            res.status(401).json({
+                message: 'Пароли не совпадают, попробуйте снова!!!'
+            })
         }
-    })
+    }else{
+        //Пользователя нет, ошибка
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден!!!'
+        })
+    }
 }
 module.exports.register = async (req,res)=>{
     //Ищем пользователя в базе
