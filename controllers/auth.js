e6a136acfb685ede04a790c48ca33d6754816cc8
YuriyPimenov <github1991>
2018-08-19 12:51:12 +0500

    Регистрация, bcryptjs-шифровка паролей

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+//Для паролей
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 
 module.exports.login = (req,res)=>{
@@ -8,10 +10,31 @@ module.exports.login = (req,res)=>{
         }
     })
 }
-module.exports.register = (req,res)=>{
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
-    user.save().then(()=>console.log('User created'))
+module.exports.register = async (req,res)=>{
+    //Ищем пользователя в базе
+    const candidate = await User.findOne({email: req.body.email})
+
+    if(candidate){
+        //Если он есть в базе, то кидаем ошибку
+        res.status(409).json({
+            message: 'Такой email уже существует!!!'
+        })
+    }else{
+        //Иначе создаём его
+
+        //Генерируем хэш
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try{
+            await user.save()
+            res.status(201).json(user)
+        }catch(e){
+            //Обработка ошибок
+        }
+    }
 }
