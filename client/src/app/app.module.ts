d7eb383121ba93311fe77cf18387dce842ec9b9b
YuriyPimenov <github1991>
2018-09-09 21:54:09 +0500

    Установка Router

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import {AppRoutingModule} from './app-routing.module'
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,HttpModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
